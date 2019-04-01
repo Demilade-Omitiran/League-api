ffa8f8fe2554edae08b0c92413cf2c2b86112402
Demilade7 <omitirand@gmail.com>
2019-04-01 21:32:25 +0100

    Add json_render methods and root route/endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  def index
+    global_json_render(200, "API works")
+  end
+
+  protected
+
+  def global_json_render(status, message, data = {}, meta = {})
+    json_payload = Hash.new
+    json_payload[:message] = message
+    if (status == 200 || status == 201)
+      json_payload[:data] = data unless data.empty?
+      json_payload[:meta] = meta unless meta.empty?
+    end
+
+    render json: json_payload, status: status
+  end
+
+  def global_error_render(status, message, error)
+    json_payload = Hash.new
+    json_payload[:message] = message
+    json_payload[:errors] = error
+
+    render json: json_payload, status: status
+  end
 end

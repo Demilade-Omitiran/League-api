03300b69a1fa9eb86078c7139ed903671cadcf71
Demilade7 <omitirand@gmail.com>
2019-04-04 19:30:18 +0100

    Add spec/support directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,8 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.include RequestSpecHelper, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures

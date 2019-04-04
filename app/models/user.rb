50a43da0b5a6637374686da52c1d4cfeb15b3b50
Demilade7 <omitirand@gmail.com>
2019-04-04 19:29:59 +0100

    Remove devise registerable module from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :validatable
 
   validates_presence_of :first_name, :last_name, :password
 end

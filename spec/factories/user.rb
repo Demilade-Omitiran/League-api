0129ebf8cae278290f8d03ac2d5ca10089ebd42e
Demilade7 <omitirand@gmail.com>
2019-04-04 19:30:32 +0100

    Add user factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,17 @@
+# spec/factories/users.rb
+FactoryBot.define do
+  factory :admin do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    admin { true }
+  end
+
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+  end
+end

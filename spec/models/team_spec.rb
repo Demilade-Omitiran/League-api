d9dbd68b0b44c3e0d3463d36393839c1111d72ba
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 16:04:47 +0100

    Add validation of name uniqueness

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 end

d4381b13113e22fbce2ff045b41ebe2e2121c0e2
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 11:23:26 +0100

    Add length validation on name attribute

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
     validates_presence_of :name
+    validates_length_of :name, within: 4..30, too_long: 'Enter a shorter name', too_short: 'Enter a longer name'
 end

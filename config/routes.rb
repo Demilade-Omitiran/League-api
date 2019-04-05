6d1f24aeba803ee60b9046c68735f703c42e1494
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 16:00:40 +0100

    Add update_password route. Add resources 'team'

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: "application#index"
+
+  # Auth
   post '/register', to: "authentication#register"
   post '/login', to: "authentication#login"
   post '/logout', to: "authentication#logout"
+  post '/update_password', to: "authentication#update_password"
 
+  # Users
   get '/users', to: "users#index"
   get '/users/:user_id', to: "users#show"
   get '/user', to: "users#show_user"
   post '/user/update', to: "users#update"
+
+  # Teams
+  resources :teams, except: [:edit, :new], param: :team_id
+
 end

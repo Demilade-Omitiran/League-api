504a19d5536210c4e1a8bc67ead7782a8b921b03
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 21:42:19 +0100

    validate password only during create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :validatable
 
-  validates_presence_of :first_name, :last_name, :password, on: :create
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :password, on: :create
 end

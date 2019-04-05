b0a69458c620e5e92ecd65e0ec73e81893911cfb
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 21:42:36 +0100

    Remove admin block from user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,5 @@
-# spec/factories/users.rb
+# spec/factories/user.rb
 FactoryBot.define do
-  factory :admin do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password }
-    admin { true }
-  end
-
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }

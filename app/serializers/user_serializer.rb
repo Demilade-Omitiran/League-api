ebc443bda12659a4336711b50bea2ad9110ce8fc
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 15:59:16 +0100

    Add timestamps to the returned attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :first_name, :last_name
+  attributes :id, :email, :first_name, :last_name, :created_at, :updated_at
 end

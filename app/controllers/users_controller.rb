c19300fc928063d62ebafac1dce7ce48a76ca841
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 15:58:26 +0100

    Fix bugs in index and get_user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user_from_headers!
   before_action :check_admin!, only: [:show, :index]
   before_action :set_user, except: [:show]
 
@@ -11,20 +12,21 @@ class UsersController < ApplicationController
   end
 
   def index
-    page = params[:page] || 1
-    per_page = params[:per_page] || 20
-    # users = User.all.paginate(page: page, per_page: per_page)
-    users = User.all.paginate(page: page, per_page: per_page)
+    params[:page] ||= 1
+    params[:per_page] ||= 20
+
+    users = User.paginate(page: params[:page].to_i, per_page: params[:per_page].to_i)
+    data = serialized_users(users)
 
     counter = users.total_entries
     meta = {
       total: counter,
-      per_page: per_page.to_i,
-      page: page.to_i,
-      page_count: counter.zero? ? 1 : (counter / per_page.to_f).ceil
+      per_page: params[:per_page].to_i,
+      page: params[:page].to_i,
+      page_count: counter.zero? ? 1 : (counter / params[:per_page].to_f).ceil
     }
 
-    global_json_render(200, "Users retrieved successfully", serialized_users(users), meta)
+    global_json_render(200, "Users retrieved successfully", data, meta)
   end
 
   def show_user
@@ -38,12 +40,13 @@ class UsersController < ApplicationController
   private
 
   def get_user(user_id)
-    user = User.find(user_id)
+    user = User.find_by(id: user_id)
+    return global_error_render(404, "User not found") unless user
     global_json_render(200, "User retrieved successfully", serialized_user(user))
   end
 
   def user_update_params
-    params.permit(:first_name, :last_name, :password)
+    params.permit(:first_name, :last_name)
   end
 
   def serialized_user(user)

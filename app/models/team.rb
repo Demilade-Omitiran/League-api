971b34a6e1ffaa7a8f839c28f4a8ddb9b4f96ec7
Demilade-Omitiran <omitirand@gmail.com>
2019-04-05 21:44:38 +0100

    Validate name during save,create and update action

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
     validates_presence_of :name
-    validates_length_of :name, within: 4..30, too_long: 'Enter a shorter name', too_short: 'Enter a longer name'
+    validates_length_of :name, within: 4..30, too_long: 'Enter a shorter name', too_short: 'Enter a longer name', on: [:save, :update, :create]
     validates_uniqueness_of :name
 end

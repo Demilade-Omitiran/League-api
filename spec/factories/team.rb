5955a851ef0387bde9b3f0b0917488d43d7a3e53
Demilade-Omitiran <omitirand@gmail.com>
2019-04-06 23:46:17 +0100

    Append integer to generated team name

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -1,6 +1,6 @@
 # spec/factories/team.rb
 FactoryBot.define do
   factory :team do
-    name { Faker::Football.unique.team }
+    name { Faker::Football.team + "_" + ((1..10000).to_a).sample.to_s }
   end
 end

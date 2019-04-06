2ef9b2ca5d41e3318d16fb150d57c4df26116109
Demilade-Omitiran <omitirand@gmail.com>
2019-04-06 23:44:43 +0100

    Edit team_spec. Add example for team_fixtures.

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'Teams', type: :request do
     login_params = { email: @user.email, password: @user.password }
     @teams = FactoryBot.create_list(:team, 10)
     @first_team = @teams.first
+    @second_team = @teams.second
+    @third_team = @teams.third
     @last_team = @teams.last
 
     post '/login', params: login_params
@@ -14,12 +16,8 @@ RSpec.describe 'Teams', type: :request do
     @request_header = { 'HTTP_AUTH_TOKEN' => "Bearer #{auth_token}" }
   end
 
-  after(:all) do
-    Faker::UniqueGenerator.clear
-  end
-
   describe "index", :list_teams do
-    context "page and per_page not specified" do
+    context "page and per_page are not specified" do
       before { get '/teams', headers: @request_header }
 
       it 'returns status code 200' do
@@ -32,19 +30,18 @@ RSpec.describe 'Teams', type: :request do
 
       it 'returns the list of teams' do
         expect(json).not_to be_empty
-        expect(json['data'].size).to eq(10)
       end
 
       it 'returns meta data' do
         expect(json['meta']).not_to be_empty
-        expect(json['meta']['total']).to eq(10)
+        expect(json['meta']).to include('total')
         expect(json['meta']['page']).to eq(1)
         expect(json['meta']['per_page']).to eq(20)
         expect(json['meta']['page_count']).to eq(1)
       end
     end
 
-    context "page and per_page specified" do
+    context "when page and per_page are specified" do
       let(:page_params) { {page: 2, per_page: 4} }
       before { get '/teams', params: page_params, headers: @request_header }
 
@@ -63,7 +60,7 @@ RSpec.describe 'Teams', type: :request do
 
       it 'returns meta data' do
         expect(json['meta']).not_to be_empty
-        expect(json['meta']['total']).to eq(10)
+        expect(json['meta']).to include('total')
         expect(json['meta']['page']).to eq(2)
         expect(json['meta']['per_page']).to eq(4)
         expect(json['meta']['page_count']).to eq(3)
@@ -75,7 +72,7 @@ RSpec.describe 'Teams', type: :request do
     let(:team_id) { @first_team.id }
     let(:invalid_team_id) { 'a' }
 
-    context "team exists" do
+    context "when team exists" do
       before { get "/teams/#{team_id}", headers: @request_header }
 
       it 'returns status code 200' do
@@ -94,14 +91,14 @@ RSpec.describe 'Teams', type: :request do
       end
     end
 
-    context "team does not exist" do
+    context "when team does not exist" do
       before { get "/teams/#{invalid_team_id}", headers: @request_header }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
-      it 'returns a success message' do
+      it 'returns a failure message' do
         expect(json['message']).to eq('Team not found')
       end
     end
@@ -111,7 +108,7 @@ RSpec.describe 'Teams', type: :request do
     let(:valid_team_attributes) { attributes_for(:team) }
     let(:invalid_team_attributes) { { title: "title" } }
 
-    context "team creation successful" do
+    context "when team creation is successful" do
       before { post '/teams', params: valid_team_attributes, headers: @request_header }
 
       it 'returns status code 201' do
@@ -130,7 +127,7 @@ RSpec.describe 'Teams', type: :request do
       end
     end
 
-    context "team creation unsuccessful" do
+    context "when team creation is unsuccessful" do
       before { post '/teams', params: invalid_team_attributes, headers: @request_header }
 
       it 'returns status code 400' do
@@ -152,7 +149,7 @@ RSpec.describe 'Teams', type: :request do
     let(:empty_team_attributes) { { name: "" } }
     let(:invalid_team_attributes) { { title: "title" } }
 
-    context "team exists" do
+    context "when team exists" do
       context "team update successful" do
         before { patch "/teams/#{@first_team.id}", params: valid_team_attributes, headers: @request_header }
   
@@ -204,7 +201,7 @@ RSpec.describe 'Teams', type: :request do
       end
     end
 
-    context "team does not exist" do
+    context "when team does not exist" do
       before { patch "/teams/a", params: valid_team_attributes, headers: @request_header }
       
       it 'returns status code 404' do
@@ -240,6 +237,108 @@ RSpec.describe 'Teams', type: :request do
 
     context "team does not exist" do
       before { delete "/teams/a", headers: @request_header }
+      
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a failure message' do
+        expect(json['message']).to eq('Team not found')
+      end
+    end
+  end
+
+  describe "team_fixtures", :team_fixtures do
+    let(:valid_params_for_all) { { team_id: @first_team.id } }
+    let(:valid_params_for_home) { { team_id: @first_team.id, fixtures: 'home' } }
+    let(:valid_params_for_away) { { team_id: @first_team.id, fixtures: 'away' } }
+    let(:invalid_params) { { team_id: 'a' } }
+    context "when team exists" do
+      before do
+        create(:fixture, home_team: @first_team, away_team: @second_team)
+        create(:fixture, home_team: @first_team, away_team: @third_team)
+        create(:fixture, home_team: @last_team, away_team: @first_team)
+      end
+
+      context "when retrieving all fixtures" do
+        before { get "/teams_fixtures", params: valid_params_for_all, headers: @request_header }
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'returns a success message' do
+          expect(json['message']).to eq("All fixtures for #{@first_team.name} retrieved successfully")
+        end
+
+        it 'returns the team record' do
+          expect(json['data']['team']).not_to be_empty
+          expect(json['data']['team']['id']).to eq(@first_team.id)
+          expect(json['data']['team']['name']).to eq(@first_team.name)
+          expect(json['data']['team']['created_at']).not_to be_empty
+          expect(json['data']['team']['updated_at']).not_to be_empty
+        end
+
+        it "returns the fixtures" do
+          expect(json['data']['team']['fixtures']).not_to be_empty
+          expect(json['data']['team']['fixtures'].size).to eq(3)
+        end
+      end
+
+      context "when retrieving only home fixtures" do
+        before { get "/teams_fixtures", params: valid_params_for_home, headers: @request_header }
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'returns a success message' do
+          expect(json['message']).to eq("Home fixtures for #{@first_team.name} retrieved successfully")
+        end
+
+        it 'returns the team record' do
+          expect(json['data']['team']).not_to be_empty
+          expect(json['data']['team']['id']).to eq(@first_team.id)
+          expect(json['data']['team']['name']).to eq(@first_team.name)
+          expect(json['data']['team']['created_at']).not_to be_empty
+          expect(json['data']['team']['updated_at']).not_to be_empty
+        end
+
+        it "returns the fixtures" do
+          expect(json['data']['team']['fixtures']).not_to be_empty
+          expect(json['data']['team']['fixtures'].size).to eq(2)
+        end
+      end
+
+      context "when retrieving only away fixtures" do
+        before { get "/teams_fixtures", params: valid_params_for_away, headers: @request_header }
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'returns a success message' do
+          expect(json['message']).to eq("Away fixtures for #{@first_team.name} retrieved successfully")
+        end
+
+        it 'returns the team record' do
+          expect(json['data']['team']).not_to be_empty
+          expect(json['data']['team']['id']).to eq(@first_team.id)
+          expect(json['data']['team']['name']).to eq(@first_team.name)
+          expect(json['data']['team']['created_at']).not_to be_empty
+          expect(json['data']['team']['updated_at']).not_to be_empty
+        end
+
+        it "returns the fixtures" do
+          expect(json['data']['team']['fixtures']).not_to be_empty
+          expect(json['data']['team']['fixtures'].size).to eq(1)
+        end
+      end
+
+    end
+
+    context "when team does not exist" do
+      before { get "/teams_fixtures", params: invalid_params, headers: @request_header }
       
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
